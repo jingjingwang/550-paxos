f3c85f2b47e1e22508b9ecc4f560436e8a4ae81f
Jingjing Wang <co2@co2-OptiPlex-790.(none)>
2012-11-06 00:53:59 -0800

    more comments

--- PaxosServer.java
+++ PaxosServer.java
@@ -28,14 +28,41 @@ import java.net.ConnectException;
 
 public class PaxosServer 
 {
+	// ----- the configuration section, all the things that you might want to change are below -----
+
+	// serverPortBase: each node in Paxos has a ServerSocket, which is for other servers connecting
+	// it. The base number is serverPortBase, where server 0 has 2139, server 1 has 2140, server 2
+	// has 2141, and so on. If a server wants to connect to another server with serverID = i, it will
+	// connect to 127.0.0.1:(serverPortBase + i).
 	private static final int serverPortBase = 2139;
+
+	// clientPortBase: each node in Paxos has a ServerSocket, which is for clients connecting
+	// it. The base number is clientPortBase, where server 0 has 4139, server 1 has 4140, server 2
+	// has 4141, and so on. If a client wants to connect to a server with serverID = i, it will
+	// use: "telnet 127.0.0.1 (clientPortBase + i)".
 	private static final int clientPortBase = 4139;
+
+	// MaxClientNum: the maximum number of clients connected to this server
 	private static final int MaxClientNum = 100;
+
+	// MaxServerNum: the maximum number of servers connected to this server
 	private static final int MaxServerNum = 20;
-	private static final int cmdLength = 50;
+
+	// MaxCmdLength: the maximum length of a command 
+	private static final int MaxCmdLength = 50;
+
+	// MaxWaitingRound: the maximum number of rounds falling behind that a node can stand 
+	// before starting asking other nodes
 	private static final int MaxWaitingRound = 1;
+
+	// MaxWaitingSelectTime: the timeout of a select, after that a proposer will try to propose
+	// again if nothing changes during the selection
 	private static final long MaxWaitingSelectTime = 50;
+
+	// GeneralLossRate: the general package loss rate. 0.4 means 40% possibility to lose a package 
 	private static final double GeneralLossRate = 0.4;
+
+	// Following are different loss rate for different messages. Change them if you want a more tricky test!
 	private static final double PrepareLossRate = GeneralLossRate;
 	private static final double RePrepareLossRate = GeneralLossRate;
 	private static final double AcceptLossRate = GeneralLossRate;
@@ -44,17 +71,30 @@ public class PaxosServer
 	private static final double AskLossRate = GeneralLossRate;
 	private static final double AnswerLossRate = GeneralLossRate;
 
+	// ----- the configuration section, all the things that you might want to change are above -----
+
+	// its unique server ID
 	private static int serverID;
+	// total number of servers, given when launching
 	private static int numServer;
+	// the number of majority, derived from numServer
 	private static int numMajority;
+	// if it has proposed for this instance
 	private static boolean proposed = false;
+	// the current proposal number
 	private static int cntPropNum = 0;
+	// the current instance ID
 	private static int cntInsID = 1;
+	// the current number of connected clients
 	private static int cntNumClient = 0;
+	// the current number of connected servers
 	private static int cntNumServer = 0;
+	// the current ID of the next client, since we need to distinguish clients because they own locks
 	private static int localClientID = 0;
+	// the highetst instance ID that it has seen, will help decide the next instance ID
 	private static int highestInsID = 1;
 
+	// states that a Paxos node should keep
 	private static ExtendedHashMap<Integer, Integer> numAccepted = new ExtendedHashMap<Integer, Integer>(0);
 	private static ExtendedHashMap<Integer, Integer> numPrepareResponse = new ExtendedHashMap<Integer, Integer>(0);
 	private static ExtendedHashMap<Integer, Integer> highestAcceptedPropNum = new ExtendedHashMap<Integer, Integer>(-1);
@@ -63,14 +103,22 @@ public class PaxosServer
 	private static ExtendedHashMap<Integer, String> highestRePrepareValue = new ExtendedHashMap<Integer, String>("");
 	private static ExtendedHashMap<Integer, Integer> highestRespondedPropNum = new ExtendedHashMap<Integer, Integer>(-1);
 	
+	// the state machine
 	private static StateMachine stateMachine = new StateMachine();
+	// the file for outputting logs
 	private static BufferedWriter debuggingLog;
+	// loggingLevel, 0 is the most important one. logs having level <= loggingLevel will be outputted.
 	private static int loggingLevel = 1;
 
+	// the non-blocking stuff
 	private static Selector selector;
+	// the queue of client requests
 	private static LinkedList<ClientCommand> clientRequestQueue = new LinkedList<ClientCommand>();
+	// the set of SelectionKeys when connecting to other servers as a client. need them for broadcasting message
 	private static HashSet<SelectionKey> connAsClient = new HashSet<SelectionKey>();
+	// the set of client connections where their requests have been chosen but the responses are not ready yet
 	private static LinkedList<PendingAnswer> pendingToAnswer = new LinkedList<PendingAnswer>();
+	// the write queue for each socket
 	private static HashMap<SelectionKey, LinkedList<String> > writeQueue = new HashMap<SelectionKey, LinkedList<String> >();
 
 	private static void outputDebuggingInfo(String str, int level)
@@ -81,6 +129,7 @@ public class PaxosServer
 		if (level == 0)
 			System.out.println(str);
 		if (level <= loggingLevel)
+		// 0 is the most important one
 		{
 			debuggingLog.write(str);
 			debuggingLog.newLine();
@@ -96,28 +145,29 @@ public class PaxosServer
 
 	private static void newRoundInit()
 	{
-		//System.out.println("newRoundInit");
-		// my definition of highestInsID: highest ins id that I know that indeed has a chosen value (via "chosen" or "answer")
-		// so if each server only uses this value + 1 as the new ins id, there won't be an empty instance id with no value chosen
-
+		// init for a new round
+		outputDebuggingInfo("newRoundInit", 1);
 		if (stateMachine.getInput(cntInsID).equals("none")) 
 		{
-			// not a new round, update proposer's state
-			// probably a good motivation to seperate 3 roles here
+			// hasn't had a chosen value, not a new round, so just update proposer's state
 			numAccepted.put(cntInsID, 0);
 			numPrepareResponse.put(cntInsID, 0);
 			cntPropNum += (new Random()).nextInt(10) + 1;
 		}
-		else // has result for this instance already, could go to a new one
+		else 
 		{
+			// has result for this instance already, could go to a new one
 			if (getCntRequest() != null && stateMachine.getInput(cntInsID).equals(getCntRequest())) 
-			// the value is successfully chosen, move to the next request
 			{
+				// the request is successfully chosen, move to the next request
 				outputDebuggingInfo("add pendig answer " + cntInsID + " " + clientRequestQueue.get(0).command, 2);
 				pendingToAnswer.add(new PendingAnswer(clientRequestQueue.get(0), cntInsID));
+				// not needed anymore
 				clientRequestQueue.remove();
 				checkPendingAnswer();
 			}
+			// highestInsID: highest instance ID that I know via "chosen" or "answer"
+			// to guarantee there won't be an empty instance id with no value chosen
 			cntInsID = highestInsID + 1;
 			cntPropNum = (new Random()).nextInt(10) + 1;
 		}
@@ -135,6 +185,7 @@ public class PaxosServer
 			String tmpAns = stateMachine.getOutput(tmpID);
 			if (tmpAns != null)
 			{
+				// could respond to this pending client
 				addIntoWriteQueue(pendingToAnswer.get(i).clientCommand.key, tmpAns + '\n'); 
 				pendingToAnswer.remove(i);
 			}
@@ -152,6 +203,7 @@ public class PaxosServer
 
 	public static void checkIfAskMissedInstance(int flyingInsID)
 	{
+		// if it is falling behind for a certain number of iterations
 		if (flyingInsID - stateMachine.nextProcessInsID >= MaxWaitingRound)
 		{
 			outputDebuggingInfo("asking from " + stateMachine.nextProcessInsID + " to " + (flyingInsID-1), 2);
@@ -179,11 +231,12 @@ public class PaxosServer
 		}
 	}
 
-	public static void newClientRequest(String str, SelectionKey key) //(ClientCommand cmd)
+	public static void newClientRequest(String str, SelectionKey key)
 	{
 		outputDebuggingInfo("new client request" , 1);
 		String tmp = (String)(key.attachment());
 		str = str + ":" + tmp.substring(tmp.indexOf("_")+1);
+		// we've got the client request, block it until the response is sent
 		key.interestOps(key.interestOps() ^ SelectionKey.OP_READ);
 		clientRequestQueue.add(new ClientCommand(str, key));
 		if (!proposed)
@@ -235,13 +288,16 @@ public class PaxosServer
 	{
 		try
 		{
+			// currently has to be 127.0.0.1 with a unique port number indicating the serverID
 			SocketChannel tmp = createSocketChannel("127.0.0.1", serverPortBase + i);
 			if (tmp != null)
 			{
+				// connected to an existing server
 				SelectionKey key = tmp.register(selector, SelectionKey.OP_READ);
 				key.attach("paxos_as_client");
 				connAsClient.add(key);
 				if (justStart)
+					// not because of receiving a "newserver" message
 					writeToSocketChannel(key, extendCommand(0, "newserver " + serverID));
 			}
 		}
@@ -282,26 +338,29 @@ public class PaxosServer
 
 		SocketChannel channel = (SocketChannel)key.channel();
 		ByteBuffer single = ByteBuffer.allocateDirect(1);
-		ByteBuffer buffer = ByteBuffer.allocateDirect(cmdLength);
+		ByteBuffer buffer = ByteBuffer.allocateDirect(MaxCmdLength);
 		single.clear();
 		buffer.clear();
 
 		int hasRead = 0;
 		while (true)
 		{
+			// read bytes one by one is safe
 			int justRead = channel.read(single);
 			if (justRead == -1) 
 				throw new Exception();
 			if (justRead > 0)
 			{
 				hasRead += justRead;
+				// a client request from telnet ends with 13 & 10
+				// a message sent by other servers ends with #
 				if (single.get(0) == (byte)(10) || single.get(0) == (byte)('#'))
 					break;
 				buffer.put(single.get(0));
 			}
-			if (hasRead >= cmdLength)
+			if (hasRead >= MaxCmdLength)
 			{
-				outputDebuggingInfo("client command too long!", 0);
+				outputDebuggingInfo("command too long!", 0);
 				break;
 			}
 			single.clear();
@@ -311,13 +370,16 @@ public class PaxosServer
         	Charset charset = Charset.defaultCharset();  
         	CharsetDecoder decoder = charset.newDecoder();  
         	String s = decoder.decode(buffer).toString().trim();
-		if (single.get(0) == (byte)(10)) // from client
+		if (single.get(0) == (byte)(10)) 
+			// we need to make the requests sent by different clients on different servers different,
+			// even if they look all the same like "lock(x)"
 			s = s + ":" + serverID;
 		return s;
 
 		}
 		catch (Exception e)
 		{
+			// the connection doesn't exist anymore
 			outputDebuggingInfo("client connection closed, read", 1);
 			removeConnection(key);
 			return "";
@@ -348,6 +410,7 @@ public class PaxosServer
 		}
 		catch (Exception e)
 		{
+			// the connection doesn't exist anymore
 			outputDebuggingInfo("connection closed, write", 1);
 			removeConnection(key);
 		}
@@ -361,12 +424,14 @@ public class PaxosServer
 		if (writeQueue.get(key) == null)
 			writeQueue.put(key, new LinkedList<String>());
 		writeQueue.get(key).add(command);
+		// we are interested in OP_WRITE iff there is something to write
 		key.interestOps(key.interestOps() | SelectionKey.OP_WRITE);
 	}
 	
 	public static String popFromWriteQueue(SelectionKey key)
 	{
 		if (key.isValid() && writeQueue.get(key).size() == 1)
+			// we are interested in OP_WRITE iff there is something to write
 			key.interestOps(key.interestOps() ^ SelectionKey.OP_WRITE);
 		String command = writeQueue.get(key).remove();
 		outputDebuggingInfo("popfromwritequeue " + command, 2);
@@ -401,6 +466,7 @@ public class PaxosServer
 		}
 		catch (FileNotFoundException e)
 		{
+			// if there is no checkpoint 
 		}
 		catch (Exception e)
 		{
@@ -475,6 +541,7 @@ public class PaxosServer
 			Iterator<SelectionKey> keyIter = selector.selectedKeys().iterator();
 			while (keyIter.hasNext())
 			{
+				// handle keys one by one
 				SelectionKey selKey = keyIter.next();
 				if (selKey.readyOps() > 0)
 			 		processSelectionKey(selKey);
@@ -483,7 +550,7 @@ public class PaxosServer
 		    	}
 			outputDebuggingInfo("------ one selection ------", 2);
 			if (numChanged == 0 && proposed)
-				//waited so long, trying to propose again
+				//waited so long with no updates, trying to propose again
 				newRoundInit();
 		}
 		
@@ -527,6 +594,7 @@ public class PaxosServer
 
 			if (tag.startsWith("paxos")) // read from another server
 			{
+				// the instance ID in this message, might be different from the current one
 				int flyingInsID = Integer.parseInt(getField(command, -1));
 				if (command.startsWith("prepare"))
 				{
@@ -534,6 +602,7 @@ public class PaxosServer
 						return;
 					checkIfAskMissedInstance(flyingInsID);
 					int propNum = Integer.parseInt(getField(command, 1));
+					// Paxos logic	
 					if (propNum <= highestRespondedPropNum.getInt(flyingInsID))
 						return;
 					highestRespondedPropNum.put(flyingInsID, propNum);
@@ -547,11 +616,13 @@ public class PaxosServer
 					numPrepareResponse.put(flyingInsID, numPrepareResponse.getInt(flyingInsID) + 1);
 					int propNum = Integer.parseInt(getField(command, 1));
 					int highestAcceptedNum = Integer.parseInt(getField(command, 2));
+					// Paxos logic	
 					if (highestAcceptedNum > highestRePrepareNum.getInt(flyingInsID))
 					{
 						highestRePrepareNum.put(flyingInsID, propNum);
 						highestRePrepareValue.put(flyingInsID, getField(command, 3)); 
 					}
+					// Paxos logic	
 					if (numPrepareResponse.getInt(flyingInsID) == numMajority)
 					{
 						String tmp;
@@ -569,6 +640,7 @@ public class PaxosServer
 					checkIfAskMissedInstance(flyingInsID);
 					int propNum = Integer.parseInt(getField(command, 1));
 					String propValue = getField(command, 2);
+					// Paxos logic	
 					if (propNum < highestRespondedPropNum.getInt(flyingInsID))
 						addIntoWriteQueue(selKey, extendCommand(flyingInsID, "re-accept rej " + propNum + " " + propValue));
 					else
@@ -587,12 +659,12 @@ public class PaxosServer
 					String propValue = getField(command, 3);
 					if (command.startsWith("re-accept rej"))
 					{
-						//since we're not doing anything, it's possible to avoid sending it in the beginning
-						//good for observation though
+						//although it's useless, we're still keeping it for observation
 					}
 					else
 					{
 						numAccepted.put(flyingInsID, numAccepted.getInt(flyingInsID) + 1);
+						// Paxos logic	
 						if (numAccepted.getInt(flyingInsID) == numMajority)
 							broadcastToAllServers(extendCommand(flyingInsID, "chosen " + getField(command, 3)));
 					}
@@ -602,13 +674,15 @@ public class PaxosServer
 					if (Math.random() < ChosenLossRate)
 						return;
 					checkIfAskMissedInstance(flyingInsID);
-
+					
 					String value = getField(command, 1);
+					// got a new chosen value
 					stateMachine.input(flyingInsID, value);
 					checkPendingAnswer();
 
 					if (flyingInsID > highestInsID)
 						highestInsID = flyingInsID;
+					// try a new round
 					newRoundInit();
 				}
 				else if (command.startsWith("ask")) 
@@ -628,6 +702,7 @@ public class PaxosServer
 					int askingInsID = Integer.parseInt(getField(command, -1));
 					if (!answer.equals("none"))
 					{
+						// got a new chosen value
 						stateMachine.input(askingInsID, answer);
 						checkPendingAnswer();
 						if (askingInsID > highestInsID) 
@@ -636,22 +711,23 @@ public class PaxosServer
 				}
 				else if (command.startsWith("newserver"))
 				{
+					// connected by a new node as a server
 					checkIfAskMissedInstance(flyingInsID);
 					int id = Integer.parseInt(getField(command, 1));
+					// try to connect to that new node
 					connectToOneServer(id, false);
 				}
 			}
 			else // read from a real client
-			{
 				if (command.length() > 0)
 					newClientRequest(command, selKey);
-			}
 	    	}
 	    	if (selKey.isValid() && selKey.isWritable()) 
 		{
 			outputDebuggingInfo("writeable", 2);
 			SocketChannel sChannel = (SocketChannel)selKey.channel();
 			writeToSocketChannel(selKey, popFromWriteQueue(selKey));
+			// we are interested in OP_READ again if just wrote to a client socket
 			selKey.interestOps(selKey.interestOps() | SelectionKey.OP_READ);
 	    	}
 	}
